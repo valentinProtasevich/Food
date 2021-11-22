de15897ab9d216bd1bc4313fc461a80d6caca441
Valentin <veksel_or_vels@mail.ru>
2021-11-22 19:53:25 +0300

    Added form

--- js/script.js
+++ js/script.js
@@ -96,8 +96,7 @@ window.addEventListener('DOMContentLoaded', () => {
   //Modal
 
   const modalTrigger = document.querySelectorAll('[data-modal]'),
-    modalCloseBtn = document.querySelector('[data-close]'),
-    modal = document.querySelector('.modal');
+        modal = document.querySelector('.modal');
 
   function openModal() {
     modal.classList.add('show');
@@ -116,10 +115,8 @@ window.addEventListener('DOMContentLoaded', () => {
     document.body.style.overflow = '';
   };
 
-  modalCloseBtn.addEventListener('click', closeModal);
-
   modal.addEventListener('click', (event) => {
-    if (event.target === modal) {
+    if (event.target === modal || event.target.getAttribute('data-close') == '') {
       closeModal();
     };
   });
@@ -130,7 +127,7 @@ window.addEventListener('DOMContentLoaded', () => {
     };
   });
 
-  //const modalTimerId = setTimeout(openModal, 10000);
+  const modalTimerId = setTimeout(openModal, 50000);
 
   function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -144,7 +141,7 @@ window.addEventListener('DOMContentLoaded', () => {
   //Use claass for Cards
 
   class MenuCard {
-    constructor (src, alt, title, descr, price, parentSelector, ...classes) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
@@ -162,7 +159,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     render() {
       const element = document.createElement('div');
-      
+
       if (this.classes.length === 0) {
         this.element = 'menu__item';
         element.classList.add(this.element);
@@ -213,4 +210,82 @@ window.addEventListener('DOMContentLoaded', () => {
     '.menu .container',
     'menu__item'
   ).render();
+
+  // Forms
+
+  const forms = document.querySelectorAll('form');
+
+  const message = {
+    loading: 'img/form/spinner.svg',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...',
+  };
+
+  forms.forEach(item => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      let statusMessage = document.createElement('img');
+      statusMessage.src= message.loading;
+      statusMessage.style.cssText = `
+        display: block;
+        margin: 0 auto;
+      `;
+      form.insertAdjacentElement('afterend', statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+
+      request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+      const formData = new FormData(form);
+
+      const object = {};
+      formData.forEach(function(value, key){
+        object[key] = value;
+      });
+
+      const json = JSON.stringify(object);
+
+      request.send(json);
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          showThanksModal(message.success);
+          statusMessage.remove();
+          form.reset();
+        } else {
+          showThanksModal(message.failure);
+        }
+      });
+    });
+  }
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector('.modal__dialog');
+
+    prevModalDialog.classList.add('hide');
+    openModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+      <div class="modal__content">
+        <div class="modal_close" data-close>×</div>
+        <div class="modal__title">${message}</div>
+      </div>
+    `;
+
+    document.querySelector('.modal').append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add('show');
+      prevModalDialog.classList.remove('hide');
+      closeModal();
+    }, 4000);
+  };
 });
